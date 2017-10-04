68efcbd33ecded87a7c97ac66d01c3f16f8c5ff8
Yury Babak <ybabak@gridgain.com>
2017-10-04 19:22:10 +0300

    Encog adapter WIP

---
+++ modules/ml/src/main/encog/org/apache/ignite/ml/encog/wav/WavReader.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.ml.encog.wav;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.ignite.IgniteException;
+
+/**
+ * Wav reader
+ */
+public class WavReader {
+    /**
+     * Read wav file to list of batch of wav frames.
+     *
+     * @param path Path.
+     * @param numOfFramesInBatch Number of frames in batch.
+     */
+    public static List<double[]> read(String path, int numOfFramesInBatch){
+        List<double[]> batchs = new LinkedList<>();
+
+        try {
+            WavFile wavFile = WavFile.openWavFile(new File(path));
+
+            wavFile.display();
+
+            int numChannels = wavFile.getNumChannels();
+            double[] buf = new double[numOfFramesInBatch * numChannels];
+
+            int framesRead;
+
+            do {
+                framesRead = wavFile.readFrames(buf, numOfFramesInBatch);
+
+                batchs.add(buf.clone());
+            } while (framesRead != 0);
+
+            wavFile.close();
+        }
+        catch (IOException | WavFileException e) {
+            throw new IgniteException("Failed to read file: " + path, e);
+        }
+
+        return batchs;
+    }
+
+}

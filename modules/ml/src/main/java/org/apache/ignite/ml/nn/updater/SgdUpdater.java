5f16386eefb2da9d8c659c9f24c19a247a4f4471
YuriBabak <y.chief@gmail.com>
2017-10-25 13:38:49 +0300

    IGNITE-6386: Introduction of distributed neural networks.  replace of dl4j, broken example

--- modules/ml/src/main/java/org/apache/ignite/ml/nn/updater/SgdUpdater.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/nn/updater/SgdUpdater.java
@@ -7,7 +7,7 @@ import org.nd4j.linalg.learning.GradientUpdater;
 public class SgdUpdater extends BaseUpdater {
     @Override
     public GradientUpdater init(String variable, Layer layer) {
-        org.nd4j.linalg.learning.Sgd updater = (org.nd4j.linalg.learning.config.Sgd) updaterForVariable.get(variable);
+        org.nd4j.linalg.learning.Sgd updater = (org.nd4j.linalg.learning.Sgd) updaterForVariable.get(variable);
         if(updater == null) {
             updater = new org.nd4j.linalg.learning.Sgd(layer.conf().getLearningRateByParam(variable));
             updaterForVariable.put(variable,updater);
--- modules/ml/src/main/java/org/apache/ignite/ml/nn/weights/WeightInitUtil.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/nn/weights/WeightInitUtil.java
@@ -51,3 +51,4 @@ public class WeightInitUtil {
         return paramsView.reshape(flatteningOrder, shape);
     }
 }
+

b7504cb3576d0a4ddf3f2d0e33748c34243a4e8f
Yury Babak <ybabak@gridgain.com>
2018-12-10 07:55:53 +0300

    trd commit

--- modules/yardstick/src/main/java/org/apache/ignite/yardstick/ml/SVMBenchmark.java
+++ modules/yardstick/src/main/java/org/apache/ignite/yardstick/ml/SVMBenchmark.java
@@ -22,49 +22,106 @@ import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 import javax.cache.Cache;
+import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
 import org.apache.ignite.cache.query.QueryCursor;
 import org.apache.ignite.cache.query.ScanQuery;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.ml.Model;
+import org.apache.ignite.ml.math.functions.IgniteBiFunction;
 import org.apache.ignite.ml.math.primitives.vector.Vector;
 import org.apache.ignite.ml.math.primitives.vector.VectorUtils;
 import org.apache.ignite.ml.svm.SVMLinearBinaryClassificationModel;
 import org.apache.ignite.ml.svm.SVMLinearBinaryClassificationTrainer;
 import org.apache.ignite.yardstick.cache.IgniteCacheAbstractBenchmark;
 import org.yardstickframework.BenchmarkConfiguration;
+import org.yardstickframework.BenchmarkUtils;
 
 /**
  * Ignite benchmark for {@link SVMLinearBinaryClassificationTrainer}
  */
 public class SVMBenchmark extends IgniteCacheAbstractBenchmark<Integer, Vector> {
-    private static int DIMENTION = 2;
+    /** Dimension. */
+    private static int DIMENSION = 2;
+    /** Dataset size. */
     private static int DATASET_SIZE = 30000;
+    /** Seed. */
     private static long SEED = 123456L;
 
+    /** Model. */
     SVMLinearBinaryClassificationModel model;
 
     /** {@inheritDoc} */
     @Override public boolean test(Map<Object, Object> map) throws Exception {
         SVMLinearBinaryClassificationTrainer trainer = new SVMLinearBinaryClassificationTrainer();
 
+        Ignite ignite = ignite();
+        IgniteCache<Integer, Vector> cache = cache();
+
+        assert ignite != null;
+        assert cache != null;
+
+        IgniteBiFunction featureExtractor =
+            (IgniteBiFunction<Integer, Vector, Vector>)(integer, vector) -> vector.copyOfRange(1, vector.size());
+        IgniteBiFunction lbExtractor =
+            (IgniteBiFunction<Integer, Vector, Double>)(integer, vector) -> vector.get(0);
+
         model = trainer.fit(
-            ignite(),
-            cache(),
-            (k, v) -> v.copyOfRange(1, v.size()),
-            (k, v) -> v.get(0)
+            ignite,
+            cache,
+            featureExtractor,
+            lbExtractor
         );
 
         return false;
     }
 
+    /** {@inheritDoc} */
     @Override public void tearDown() throws Exception {
         modelEvaluation(model);
 
         super.tearDown();
     }
 
+    /** {@inheritDoc} */
+    @Override protected IgniteCache<Integer, Vector> cache() {
+        CacheConfiguration<Integer, Vector> cacheConfiguration = new CacheConfiguration<>();
+        cacheConfiguration.setName("YARDSTICK_" + UUID.randomUUID());
+        cacheConfiguration.setAffinity(new RendezvousAffinityFunction(false, 10));
+
+        return ignite().getOrCreateCache(cacheConfiguration);
+    }
+
+    /** {@inheritDoc} */
+    @Override public void setUp(BenchmarkConfiguration cfg) throws Exception {
+        super.setUp(cfg);
+        fillCache();
+    }
+
+    /** Fill cache by random points. */
+    private void fillCache() {
+        IgniteCache<Integer, Vector> cache = cache();
+        Random random = new Random(SEED);
+
+        for (int i = 0; i < DATASET_SIZE; i++)
+            cache.put(i, nextPoint(random));
+    }
+
+    /* Generate a next random point. */
+    private Vector nextPoint(Random r){
+        double lb = r.nextBoolean() ? 1d : 0d;
+        double[] pnt = new double[DIMENSION + 1];
+
+        pnt[0] = lb;
+
+        for (int i = 1; i < DIMENSION + 1; i++)
+            pnt[i] = lb - r.nextDouble();
+
+        return VectorUtils.of(pnt);
+    }
+
+    /** Evaluate trained model. */
     private void modelEvaluation(Model<Vector, Double> mdl){
         int amountOfErrors = 0;
         int totalAmount = 0;
@@ -88,51 +145,12 @@ public class SVMBenchmark extends IgniteCacheAbstractBenchmark<Integer, Vector> 
 
                 confusionMtx[idx1][idx2]++;
 
-                System.out.printf(">>> | %.4f\t\t| %.4f\t\t|\n", prediction, groundTruth);
+                BenchmarkUtils.println(String.format(">>> | %.4f\t\t| %.4f\t\t|\n", prediction, groundTruth));
             }
-
-            System.out.println(">>> ---------------------------------");
-
-            System.out.println("\n>>> Absolute amount of errors " + amountOfErrors);
-            System.out.println("\n>>> Accuracy " + (1 - amountOfErrors / (double)totalAmount));
+            BenchmarkUtils.println("\n>>> Absolute amount of errors " + amountOfErrors);
+            BenchmarkUtils.println("\n>>> Accuracy " + (1 - amountOfErrors / (double)totalAmount));
         }
 
-        System.out.println("\n>>> Confusion matrix is " + Arrays.deepToString(confusionMtx));
-    }
-
-    /** {@inheritDoc} */
-    @Override protected IgniteCache<Integer, Vector> cache() {
-        CacheConfiguration<Integer, Vector> cacheConfiguration = new CacheConfiguration<>();
-        cacheConfiguration.setName("YARDSTICK_" + UUID.randomUUID());
-        cacheConfiguration.setAffinity(new RendezvousAffinityFunction(false, 10));
-
-        return ignite().getOrCreateCache(cacheConfiguration);
-    }
-
-    /** {@inheritDoc} */
-    @Override public void setUp(BenchmarkConfiguration cfg) throws Exception {
-        super.setUp(cfg);
-
-        fillCache();
-    }
-
-    private void fillCache() {
-        IgniteCache<Integer, Vector> cache = cache();
-        Random random = new Random(SEED);
-        for (int i = 0; i < DATASET_SIZE; i++)
-            cache.put(i, nextPoint(random));
-    }
-
-    private Vector nextPoint(Random r){
-        double lb = r.nextBoolean() ? 1d : 0d;
-
-        double[] pnt = new double[DIMENTION + 1];
-
-        pnt[0] = lb;
-
-        for (int i = 1; i < DIMENTION + 1; i++)
-            pnt[i] = lb - r.nextDouble();
-
-        return VectorUtils.of(pnt);
+        BenchmarkUtils.println("\n>>> Confusion matrix is " + Arrays.deepToString(confusionMtx));
     }
 }
